fa524447b8567c0d892d45dabb0dbdf980f5cae3
Alyssa Walker <alyssawalker352@gmail.com>
2022-11-12 20:45:50 -0500

    Ex.5 Completed

--- world.js
+++ world.js
@@ -1,14 +1,27 @@
 window.onload = click;
 
+var data;
+
 function click(){
     
     document.getElementById("lookup").addEventListener("click", getCountry);
+    document.getElementById("lookup_cities").addEventListener("click", getcities);
 }
 
 function getCountry(){
-    var data = document.getElementById("country").value;
+    data = document.getElementById("country").value;
     
     fetch("world.php?country=" +data)
+    .then(a => a.text())
+    .then(x=>{
+        document.getElementById("result").innerHTML = x;
+    })
+}
+
+function getcities(){
+    data = document.getElementById("country").value;
+    
+    fetch("world.php?country="+data+"&Lookup=cities")
     .then(a => a.text())
     .then(x=>{
         document.getElementById("result").innerHTML = x;
--- world.php
+++ world.php
@@ -16,24 +16,45 @@ if(isset($_GET['country']))
   $country = filter_var($input, FILTER_SANITIZE_STRING);
   //REMOVED $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
   $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+
+  $heads = array('Name', 'Continent', 'Independence Year', 'Head of State');
+
   $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  $conCode = $results[0]['code'];
+  
+  if(isset($_GET['Lookup']) && !empty($_GET['Lookup']))
+  {
+    $stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities JOIN countries ON cities.country_code = countries.code WHERE countries.code = '$conCode'");
+    $heads = array('Name', 'District', 'Population');
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
+
 }
 
 ?>
 
 <table>
   <tr>
-      <th>Name</th>
-      <th>Continent</th>
-      <th>Independence</th>
-      <th>Head of State</th>
+  <?php foreach ($heads as $column): ?>
+      <th><?= $column;?></th>
+  <?php endforeach ?>
   </tr> 
-<?php foreach ($results as $column): ?>
+<?php if(count($heads) === 4): 
+    foreach ($results as $column): ?>
   <tr>
       <td><?= $column['name'];?></td>
       <td><?= $column['continent'];?></td>
       <td><?= $column['independence_year'];?></td>
       <td><?= $column['head_of_state'];?></td>
   </tr>
-<?php endforeach; ?>
+<?php endforeach;
+else: 
+ foreach ($results as $column): ?>
+  <tr>
+      <td><?= $column['name'];?></td>
+      <td><?= $column['district'];?></td>
+      <td><?= $column['population'];?></td>
+  </tr>
+  <?php endforeach; ?>
+  <?php endif; ?>
 </table>
